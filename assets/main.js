82bdd3662613df5edc3234d6728ff180ff91130b
Lucine0508 <113482570+Lucine0508@users.noreply.github.com>
2023-02-28 19:19:07 -0500

    3

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,16 @@
 window.arenaCallback = () => {
 	// Put your JavaScript in here!
 	// This will run after Are.na’s API returns your data.
+	let imageblocks = document.querySelectorAll('li.image-block')
+	
+	imageblocks.forEach((block)=> {
+		block.onclick = () =>{
+			block.classList.add('active')
+		}
+	})
+
+	block.querySelector('aside').onclick = () =>{
+		block.classList.remove('active')
+	}
+	
 }
